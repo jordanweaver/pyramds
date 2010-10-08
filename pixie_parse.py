be44b281112142497b71b7a328e10089645e2c75
Jordan Weaver <i.jordanweaver@gmail.com>
2010-10-08 16:47:27 -0500

    Fixed gamma-gamma coincidence parser. HDF structure more like Compton. where conditions are crazy long now.

--- pixie_parse.py
+++ pixie_parse.py
@@ -11,6 +11,7 @@ import os
 import numpy as np
 import math
 from tables import *
+import sys
 
 # pyramds_cfg contains user-supplied info on detector systems and which data
 # files are to be used in the analysis
@@ -130,7 +131,7 @@ while os.path.exists(file_path + '.bin'):
         
             buffer_no += 1
             if buffer_no%100 == 0:
-                print('\bBuffer No. {0}'.format(buffer_no))
+                print('Buffer No. %d' % buffer_no)
             
             # Flush data to the HFD5 table and start new buffer    
             table.flush()
@@ -151,7 +152,7 @@ gGGcoinc = h5file.createGroup(h5file.root.spectra, "ggcoinc", "Gamma-Gamma Data"
 ################################################################################
 # Store Det-1 & 2 arrays containing normal counts###############################
 ################################################################################
-
+print('Started creating normal data aggregates...')
 norm12table = h5file.createTable(gNormal, 'norm_evts12', AggEvent2, "Normal Time-Stamped Data")
 norm_event = norm12table.row
 
@@ -194,6 +195,7 @@ h5file.createArray(gNormal, 'norm2_spec', dt_array, "Normal Time-Chunked Spec Ar
 ################################################################################
 # Store Det-1 & 2 arrays containing Compton counts##############################
 ################################################################################
+print('Started creating anti-coincidence data aggregates...')
 
 compt1table = h5file.createTable(gCompton, 'compt_evts1', AggEvent1, "Compton Time-Stamped Data - Det 1")
 compt_event = compt1table.row
@@ -244,44 +246,65 @@ h5file.createArray(gCompton, 'compt2_spec', dt_array, "Compton-Supp Time-Chunked
 ################################################################################
 # Store Det 1 & 2 Gamma-Gamma arrays containg counts############################
 ################################################################################
+print('Started creating coincidence data aggregates...')
 
-gg12table = h5file.createTable(gGGcoinc, 'gg_evts12', AggEvent2, "G-G Time-Stamped Data")
-gg_event = gg12table.row
+gg1table = h5file.createTable(gGGcoinc, 'gg_evts1', AggEvent1, "G-G Time-Stamped Data - Det 1")
+gg_event = gg1table.row
 
-for row in table.where("""(energy_0 == -1) & (energy_1 != -1) & (energy_2 != -1) & (deltaT_12 < %f)"""%short_window):
-    gg_event['energy_1'] = row['energy_1']
-    gg_event['energy_2'] = row['energy_2']
+# Det 1
+teststr1 = """((energy_0 == -1) & (energy_1 != -1) & (energy_2 != -1) & (deltaT_12 < %f))"""%short_window
+teststr2 = """((energy_0 != -1) & (energy_1 != -1) & (energy_2 == -1) & (deltaT_01 < %f))"""%short_window
+teststr3 = """((energy_0 != -1) & (energy_1 != -1) & (energy_2 != -1) & (deltaT_01 < %f))"""%short_window
+teststr4 = """((energy_0 != -1) & (energy_1 != -1) & (energy_2 != -1) & (deltaT_12 < %f))"""%short_window
+teststr = ' | '.join([teststr1,teststr2,teststr3,teststr4])
+
+for row in table.where("""%s""" % teststr):
+    gg_event['energy'] = row['energy_1']
     gg_event['timestamp'] = row['timestamp']
     gg_event.append()
     gg12table.flush()
 
-# Det 1
 dt_temp = np.zeros(energy_max + 1, dtype=np.int32)
 dt_array = np.zeros((t_array_dim + 1, energy_max + 1), dtype=np.int32)
 ti = 0
-for row in gg12table:
+for row in gg1table:
     if (row['timestamp'] - t_start) >= (ti + 1) * t_steps:
         ti += 1
         dt_array[ti] = dt_temp.copy()
-    if row['energy_1'] >= 0:
-        dt_temp[row['energy_1']] += 1
+    if row['energy'] >= 0:
+        dt_temp[row['energy']] += 1
 dt_array[-1] = dt_temp.copy()
 
 h5file.createArray(gGGcoinc, 'gg1_spec', dt_array, "G-G Time-Chunked Spec Array - Det 1")
 
 # Det 2
+gg2table = h5file.createTable(gGGcoinc, 'gg_evts2', AggEvent1, "G-G Time-Stamped Data - Det 2")
+gg_event = gg2table.row
+
+teststr1 = """((energy_0 == -1) & (energy_1 != -1) & (energy_2 != -1) & (deltaT_12 < %f))"""%short_window
+teststr2 = """((energy_0 != -1) & (energy_1 == -1) & (energy_2 != -1) & (deltaT_02 < %f))"""%short_window
+teststr3 = """((energy_0 != -1) & (energy_1 != -1) & (energy_2 != -1) & (deltaT_02 < %f))"""%short_window
+teststr4 = """((energy_0 != -1) & (energy_1 != -1) & (energy_2 != -1) & (deltaT_12 < %f))"""%short_window
+teststr = ' | '.join([teststr1,teststr2,teststr3,teststr4])
+
+for row in table.where("""%s""" % teststr):
+    gg_event['energy'] = row['energy_2']
+    gg_event['timestamp'] = row['timestamp']
+    gg_event.append()
+    gg12table.flush()
+
 dt_temp = np.zeros(energy_max + 1, dtype=np.int32)
 dt_array = np.zeros((t_array_dim + 1, energy_max + 1), dtype=np.int32)
 ti = 0
-for row in gg12table:
+for row in gg2table:
     if (row['timestamp'] - t_start) >= (ti + 1) * t_steps:
         ti += 1
         dt_array[ti] = dt_temp.copy()
-    if row['energy_2'] >= 0:
-        dt_temp[row['energy_2']] += 1
+    if row['energy'] >= 0:
+        dt_temp[row['energy']] += 1
 dt_array[-1] = dt_temp.copy()
 
-h5file.createArray(gGGcoinc, 'gg2_spec', dt_array, "G-G Time-Chunked Spec Array - Det 2")
+h5file.createArray(gGGcoinc, 'gg2_spec', dt_array, "G-G Time-Chunked Spec Array - Det 1")
 
 ################################################################################
 # Create sig_lookup strucutre in HDF5 file######################################
