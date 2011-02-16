18d15de3639a8fc2b36a47fe3cbb1c7cd70bcfc2
Jordan Weaver <i.jordanweaver@gmail.com>
2011-02-16 16:21:13 -0600

    Added marker2energy to plotting for correct axis values

--- plot_ghex.py
+++ plot_ghex.py
@@ -7,27 +7,43 @@ import tables as tb
 import matplotlib.cm as cm
 import  matplotlib.pyplot as plt
 
+from function_lib import marker2energy
+
 #file_path = raw_input('Enter path to HDF file: ')
 #file_path = '/python/PIXIE_runs/PreRevision/debug-.h5'
 #file_path = '/python/PIXIE_runs/Standards/Co60_LOW-5min.h5'
-#file_path = '/python/PIXIE_runs/Se_Test/Se_test.h5'
-file_path = '/python/PIXIE_runs/WFP_1222/WFP2_1222-3d.h5'
+file_path = '/python/PIXIE_runs/Se_Test/Se_test.h5'
+#file_path = '/python/PIXIE_runs/WFP_1222/WFP2_1222-3d.h5'
+
+en_coeff1 = [0.118379, 0.506684]
+en_coeff2 = [1.024204, 0.413356]
 
 f = tb.openFile(file_path, 'r')
 evtstab = f.root.bin_data_parse.readout
 
-emax = 700
+emax = 900
 gg_list_1 = np.array([row['energy_1'] for row in evtstab.where("""(energy_1 <= emax) & (energy_2 <= emax) & (energy_1 > 0) & (energy_2 > 0)""")])
 gg_list_2 = np.array([row['energy_2'] for row in evtstab.where("""(energy_1 <= emax) & (energy_2 <= emax) & (energy_1 > 0) & (energy_2 > 0)""")])
 
-xmin = 0
-xmax = gg_list_1.max()
-ymin = 0
-ymax = gg_list_2.max()
+gg_en_1 = np.zeros(len(gg_list_1))
+gg_en_2 = np.zeros(len(gg_list_2))
 
-hexplot = plt.hexbin(gg_list_1, gg_list_2, cmap=cm.jet, gridsize=200)
+for i in range(len(gg_list_1)):
+    gg_en_1[i] = marker2energy(gg_list_1[i], en_coeff1)
+    
+for i in range(len(gg_list_2)):
+    gg_en_2[i] = marker2energy(gg_list_2[i], en_coeff2)
+
+xmin = 0
+xmax = gg_en_1.max()
+ymin = 0
+ymax = gg_en_2.max()
+
+hexplot = plt.hexbin(gg_en_1, gg_en_2, cmap=cm.jet, gridsize=200)
 plt.axis([xmin, xmax, ymin, ymax])
 plt.title(file_path)
+plt.xlabel('Lower Detector Energy (keV)')
+plt.ylabel('Upper Detector Energy (keV)')
 cb = plt.colorbar()
 cb.set_label('Counts')
 
--- plot_gsurf.py
+++ plot_gsurf.py
@@ -8,11 +8,16 @@ import matplotlib.cm as cm
 import  matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+from function_lib import marker2energy
+
 #file_path = raw_input('Enter path to HDF file: ')
 #file_path = '/python/PIXIE_runs/PreRevision/debug-.h5'
 #file_path = '/python/PIXIE_runs/Standards/Co60_LOW-5min.h5'
-#file_path = '/python/PIXIE_runs/Se_Test/Se_test.h5'
-file_path = '/python/PIXIE_runs/WFP_1222/WFP2_1222-3d.h5'
+file_path = '/python/PIXIE_runs/Se_Test/Se_test.h5'
+#file_path = '/python/PIXIE_runs/WFP_1222/WFP2_1222-3d.h5'
+
+en_coeff1 = [0.118379, 0.506684]
+en_coeff2 = [1.024204, 0.413356]
 
 f = tb.openFile(file_path, 'r')
 evtstab = f.root.bin_data_parse.readout
@@ -21,10 +26,19 @@ emax = 700
 gg_list_1 = np.array([row['energy_1'] for row in evtstab.where("""(energy_1 <= emax) & (energy_2 <= emax) & (energy_1 > 0) & (energy_2 > 0)""")])
 gg_list_2 = np.array([row['energy_2'] for row in evtstab.where("""(energy_1 <= emax) & (energy_2 <= emax) & (energy_1 > 0) & (energy_2 > 0)""")])
 
+gg_en_1 = np.zeros(len(gg_list_1))
+gg_en_2 = np.zeros(len(gg_list_2))
+
+for i in range(len(gg_list_1)):
+    gg_en_1[i] = marker2energy(gg_list_1[i], en_coeff1)
+    
+for i in range(len(gg_list_2)):
+    gg_en_2[i] = marker2energy(gg_list_2[i], en_coeff2)
+
 xmin = 0
-xmax = gg_list_1.max()
+xmax = gg_en_1.max()
 ymin = 0
-ymax = gg_list_2.max()
+ymax = gg_en_2.max()
 
 print('Generating cust_list...')
 cust_hist = np.ones(shape=(emax,emax), dtype=np.int32)
@@ -42,4 +56,6 @@ ax = fig.gca(projection='3d')
 
 print('Starting plot...')
 ax.plot_surface(X,Y,cust_hist, linewidth=0, rstride=2, cstride=2, antialiased=False, cmap=cm.jet)
+plt.xlabel('Lower Detector Energy (keV)')
+plt.ylabel('Upper Detector Energy (keV)')
 plt.show()
