fdd853bdac1655db1de47ecdf6106750aa9978e6
Jordan Weaver <jweaver@enthought.com>
2013-11-27 15:20:35 -0600

    Add get_bin_info method; create PyTables metaclass table descriptions

--- pyramds.py
+++ pyramds.py
@@ -8,36 +8,108 @@ extraction of the necessary events used in spectra construction.
 
 """
 
-from traits.api import HasTraits, Instance, Property, File, Int
-from traitsui.api import View, Item, Button
+from datetime import datetime
 
+from traits.api import (HasTraits, Instance, Property, File, Int, Button)
+from traitsui.api import View, Item
+from tables import IsDescription, Int32Col, Float32Col
 
-class PyramdsModel(HasTraits):
-
-    # Filecounter that tracks progress through file series
-    file_counter = Int(1)
+# Setup PyTables metaclasses for use in Table constructor
+class GammaEvent(IsDescription):
     
-    # Filename for .bin file in series that is currently being processed
-    bin_file_name = File()
+    # Energy reading from Pixie detector channels 0, 1, 2
+    energy_0  = Int32Col(pos=0)
+    energy_1  = Int32Col(pos=1)
+    energy_2  = Int32Col(pos=2)
+    
+    # Time differences between detector events 
+    # (e.g. "deltaT_01" is between energy_0 and energy_1)
+    deltaT_01 = Float32Col(pos=3)
+    deltaT_02 = Float32Col(pos=4)
+    deltaT_12 = Float32Col(pos=5)
+    
+    timestamp = Float32Col(pos=6)
+
+class AggEvent1(IsDescription):
+    energy    = Int32Col(pos=0)
+    timestamp = Float32Col(pos=1)
+
+class AggEvent2(IsDescription):
+    energy_1  = Int32Col(pos=0)
+    energy_2  = Int32Col(pos=1)
+    timestamp = Float32Col(pos=2)
+
+class PyramdsParser(HasTraits):
+
+    # Filename for .bin file selected in UI
+    selected_bin_file = File()
         
     # Filename base (series number and extension removed)
-    base_filepath = Property
+    file_series = Property
     
-    def _get_base_filepath(self):
-        return self.bin_file_name[:8]
+    # Filecounter that tracks progress through file series
+    file_counter = Int(1)
+    # File path currently being processed (no extension)
+    active_file_path = file_series + '%04d'.format(file_counter)
+    
+    # Only initialize the buffer counter before the entire run... not each file.
+    buffer_no = 0
+
+    def get_bin_info(self):
+        
+        active_file_ifm = (self.file_series + '%04d' + '.ifm') % self.file_counter
+        
+        with open(active_file_ifm, 'rU') as finfo:
+    
+            info_str_list = finfo.readlines()
+    
+            date_str = info_str_list[1][23:-2]
+            date_format = '%I:%M:%S %p %a, %b %d, %Y'
+            run_start_time = datetime.strptime(date_str, date_format)
+    
+            # Total time = real time
+            total_time = info_str_list[6].split()[3]
+    
+            # Live time for each detector channel
+            live_time = [info_str_list[9 + channel].split()[2] for channel in range(4)]
+
+            self.times = {
+                'start' :   run_start_time,
+                'total' :   total_time,
+                'live'  :   live_time
+            }
+    
+            self.bufheadlen = int(info_str_list[33].split()[1])
+            self.eventheadlen = int(info_str_list[34].split()[1])
+            
+            # Due to bug in PIXIE IGOR Software, explicity set head length
+            self.chanheadlen = 2 #int(info_str_list[35].split()[1])
+        
+    
+    def _get_file_series(self):
+        return self.selected_bin_file[:8]
 
 class PyramdsView(HasTraits):
-    pmds = Instance(PyramdsModel)
+    Parser = Instance(PyramdsParser)
     
+    filename = File()  
     parse_button = Button(label="Parse Series")
 
-    traits_view = View(Item(pmds.bin_file_name))
+    traits_view = View(Item('filename', label = 'BIN File'),
+        width = 700,
+        height = 700,
+        resizable = True,
+        title = "PYRAMDS",
+        )
     
-    # On filename change, update "Parse Series" button
+    # On filename change, update parser model and pull new .ifm stats
+    def _filename_changed(self, new):
+       self.Parser.selected_bin_file = new
+       # self.parser_view.get_bin_info()
 
 if __name__ == '__main__':
-    model = PyramdsModel()
+    parser = PyramdsParser()
     
-    PyramdsWindow = PyramdsView(pmds=model)
+    PyramdsWindow = PyramdsView(Parser=parser)
     PyramdsWindow.configure_traits()
 
