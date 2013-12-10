8311a0ab3eea2d09786a69cb856d9abd6c8eb973
Jordan Weaver <jweaver@enthought.com>
2013-12-10 16:22:44 -0600

    Fixes #2 to get correct run stats from ifm files
    The get_bin_info method now loops over all ifm files in the series. It uses the run_start_time from the first file in the series and sums the values for total_time and live_time (separately for each detector) as it iterates through the ifm files.

--- pyramds.py
+++ pyramds.py
@@ -8,21 +8,23 @@ extraction of the necessary events used in spectra construction.
 
 """
 import os
+import numpy as np
 from datetime import datetime
 from fnmatch import fnmatch
-from os.path import basename, dirname, join
+from os.path import dirname, join
 
 from tables import Float32Col, Int32Col, IsDescription, openFile
-from traits.api import Button, File, HasTraits, Instance, Int, Property, Str, List
-from traitsui.api import Group, VGroup, HSplit, Item, View, ListStrEditor, FileEditor
+from traits.api import (Button, File, HasTraits, Instance, Int, List, Property)
+from traitsui.api import (FileEditor, Group, HSplit, Item, ListStrEditor,
+                          VGroup, View)
 
 # Setup PyTables metaclasses for use in Table constructor
 class GammaEvent(IsDescription):
 
     # Energy reading from Pixie detector channels 0, 1, 2
-    energy_0  = Int32Col(pos=0)
-    energy_1  = Int32Col(pos=1)
-    energy_2  = Int32Col(pos=2)
+    energy_0 = Int32Col(pos=0)
+    energy_1 = Int32Col(pos=1)
+    energy_2 = Int32Col(pos=2)
 
     # Time differences between detector events
     # (e.g. "deltaT_01" is between energy_0 and energy_1)
@@ -68,40 +70,48 @@ class PyramdsParser(HasTraits):
         # Populate file_series list
         file_wildcard = self.series_basename + '*.' + ext
 
-        for file in os.listdir(self.data_cwd):
-            if fnmatch(join(self.data_cwd, file), file_wildcard):
-                file_series.append(file)
+        for data_file in os.listdir(self.data_cwd):
+            if fnmatch(join(self.data_cwd, data_file), file_wildcard):
+                file_series.append(data_file)
         return file_series
 
     def get_bin_info(self):
 
-        active_file_ifm = self.active_file_path + ".ifm"
+        ifm_file_series = self.get_file_series('ifm')
+        ifm_file_series.sort()
 
-        with open(active_file_ifm, 'rU') as finfo:
+        total_time = 0.0
+        live_time = np.zeros(4)
 
-            info_str_list = finfo.readlines()
+        for ifm_file in ifm_file_series:
+            with open(ifm_file, 'rU') as f:
 
-            date_str = info_str_list[1][23:-2]
-            date_format = '%I:%M:%S %p %a, %b %d, %Y'
-            run_start_time = datetime.strptime(date_str, date_format)
+                info_str_list = f.readlines()
 
-            # Total time = real time
-            total_time = info_str_list[6].split()[3]
+                # On first pass/file, store the series run start time
+                if ifm_file is ifm_file_series[0]:
+                    date_str = info_str_list[1][23:-2]
+                    date_format = '%I:%M:%S %p %a, %b %d, %Y'
+                    run_start_time = datetime.strptime(date_str, date_format)
 
-            # Live time for each detector channel
-            live_time = [info_str_list[9 + channel].split()[2] for channel in range(4)]
+                # Total time = real time; increment value for each file read
+                total_time_str = info_str_list[6].split()[3]
+                total_time += float(total_time_str)
 
-            self.times = {
-                'start' :   run_start_time,
-                'total' :   total_time,
-                'live'  :   live_time
-            }
+                # Live time for each detector channel
+                live_time_str = [info_str_list[9 + channel].split()[2]
+                                 for channel in range(4)]
+                live_time += np.array(map(float, live_time_str))
 
-            self.bufheadlen = int(info_str_list[33].split()[1])
-            self.eventheadlen = int(info_str_list[34].split()[1])
+                self.bufheadlen = int(info_str_list[33].split()[1])
+                self.eventheadlen = int(info_str_list[34].split()[1])
 
-            # Due to bug in PIXIE IGOR Software, explicity set head length
-            self.chanheadlen = 2 #int(info_str_list[35].split()[1])
+                # Bug in PIXIE IGOR Software, explicity set chanheadlen
+                self.chanheadlen = 2  # int(info_str_list[35].split()[1])
+
+        self.times = {'start': run_start_time,
+                      'total': str(total_time),
+                      'live': map(str, live_time)}
 
     def create_h5(self):
         """
@@ -112,33 +122,39 @@ class PyramdsParser(HasTraits):
         h5_filename = self.series_basename + '.h5'
         h5_title = 'Data - ' + self.series_basename
 
-        self.h5file = openFile(h5_filename, mode = 'w', title = h5_title)
+        self.h5file = openFile(h5_filename, mode='w', title=h5_title)
 
-        self.h5_group = self.h5file.createGroup(self.h5file.root,
+        self.h5_group = self.h5file.createGroup(
+            self.h5file.root,
             'bin_data_parse', 'PIXIE Binary Parse')
 
-        self.h5file.createGroup(self.h5file.root,
-            'times', 'File time information')
+        self.h5file.createGroup(
+            self.h5file.root, 'times', 'File time information')
 
-        self.h5file.createGroup(self.h5file.root,
-            "spectra", "Spectra Arrays")
+        self.h5file.createGroup(
+            self.h5file.root, "spectra", "Spectra Arrays")
 
-        self.h5_gNormal = self.h5file.createGroup(self.h5file.root.spectra,
-            "normal", "Normal Data")
+        self.h5_gNormal = self.h5file.createGroup(
+            self.h5file.root.spectra, "normal", "Normal Data")
 
-        self.h5_gCompton = self.h5file.createGroup(self.h5file.root.spectra,
+        self.h5_gCompton = self.h5file.createGroup(
+            self.h5file.root.spectra,
             "compton", "Compton-Supp Data")
 
-        self.h5_gGGcoinc = self.h5file.createGroup(self.h5file.root.spectra,
+        self.h5_gGGcoinc = self.h5file.createGroup(
+            self.h5file.root.spectra,
             "ggcoinc", "Gamma-Gamma Data")
 
     def record_time_stats(self):
 
-        self.h5file.createArray(self.h5file.root.times, 'live', times['live'], "Live times of run")
+        self.h5file.createArray(
+            self.h5file.root.times, 'live', times['live'], "Live times of run")
         startt = times['start'].timetuple()
-        self.h5file.createArray(self.h5file.root.times, 'start', [x for x in startt][:-3], "Start time list of run")
-        self.h5file.createArray(self.h5file.root.times, 'total', [times['total']])
-
+        self.h5file.createArray(
+            self.h5file.root.times, 'start', [x for x in startt][:-3],
+            "Start time list of run")
+        self.h5file.createArray(
+            self.h5file.root.times, 'total', [times['total']])
 
     def _get_data_cwd(self):
         return dirname(parser.series_basename)
@@ -147,7 +163,9 @@ class PyramdsParser(HasTraits):
         return self.selected_data_file[:-8]
 
     def _get_active_file_path(self):
-        return "{self.series_basename}{self.file_counter:0>4}".format(self=self)
+        active_file_path = \
+            "{self.series_basename}{self.file_counter:0>4}".format(self=self)
+        return active_file_path
 
 class PyramdsView(HasTraits):
     Parser = Instance(PyramdsParser)
@@ -176,18 +194,18 @@ class PyramdsView(HasTraits):
         Group(
             VGroup(Item('hdf_filename', editor=bin_file_editor, label='HDF File'),
             show_border=True,),label="SPECTRUM EXPORTER"),
-        resizable = True,
-        title = "PYRAMDS",
+        resizable=True,
+        title="PYRAMDS",
         )
 
     # On filename change, update parser model and pull new .ifm stats
     def _bin_filename_changed(self, new):
 
-       self.Parser.selected_data_file = new
-       self.bin_file_series = self.Parser.get_file_series('bin')
+        self.Parser.selected_data_file = new
+        self.bin_file_series = self.Parser.get_file_series('bin')
 
-       self.ifm_file_series = self.Parser.get_file_series('ifm')
-       for ifm in self.ifm_file_series:
+        self.ifm_file_series = self.Parser.get_file_series('ifm')
+        for ifm in self.ifm_file_series:
            pass
 
 if __name__ == '__main__':
